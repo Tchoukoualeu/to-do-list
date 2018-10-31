20e12af73a363b0b623d1181354414ac62107d6b
Aris Tchoukoualeu <38705757+Tchoukoualeu@users.noreply.github.com>
2018-10-31 10:50:09 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,43 @@
+let put = $(".put");
+let bg = "#eec2c2";
+
+//Delete task
+put.on("click", "span", function(event) {
+  $(this)
+    .parent()
+    .fadeOut(350, function() {
+      $(this).remove();
+    });
+  event.stopPropagation();
+});
+
+//Eliminate task when completed
+put.on("click", ".code", function() {
+  if($(this).css("text-decoration-line") == "line-through"){
+    $(this).css("text-decoration-line", "none");
+  }else{
+    $(this).css("text-decoration-line", "line-through");
+  }
+});
+
+//Add a task
+$("textarea").keypress(function(event) {
+  if (event.which === 13) {
+    let input = $(".text").val();
+    $(".text").val("");
+    if (input.trim() !== "") {
+      put.append(
+        '<div class="code" style="background-color:' +
+          bg +
+          '">' +
+          input +
+          '<span id="close">×</span></div>'
+      );
+      if (bg == "#f2f2b0") {
+        bg = "#eec2c2";
+      } else {
+        bg = "#f2f2b0";
+      }
+    }
+  }
+});
